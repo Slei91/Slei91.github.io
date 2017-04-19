da426a2a7989ff161902e4c223d59b7b104b648b
Slei91 <danilych91@rambler.ru>
2017-04-19 22:53:53 +0300

    первый пример адаптива

---
+++ bstest/js/common.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+
+	$(".button").click(function () {
+		$(".main_nav ul").slideToggle();
+	});
+
+	//Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".fancybox").fancybox();
+
+
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+
+});
